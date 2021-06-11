98f27313f6aa1b916383874d538fabb4f9ddcc00
hackerton <9619080+HackerTon@users.noreply.github.com>
2021-06-11 14:26:52 +0800

    added learning rate scheduler
    Signed-off-by: hackerton <9619080+HackerTon@users.noreply.github.com>

--- model.py
+++ model.py
@@ -20,28 +20,15 @@ def one2hot(attr, nlabel, shape=[128, 128]):
 
     return cube
 
-    # attr = tf.cast(attr, dtype=tf.float32)
-    # cube = tf.TensorArray(tf.float32, nlabel, dynamic_size=True)
-
-    # for i in tf.range(0, 2):
-    #     filler = tf.fill(size, attr[i])
-    #     cube.write(i, filler)
-
-    # return tf.transpose(cube.stack(), [1, 2, 0])
-
 
 def resi_block(input_layer, k):
     # first block
-    d_block = tf.keras.layers.Conv2D(k, (3, 3), (1, 1), padding="same", use_bias=False)(
-        input_layer
-    )
+    d_block = tf.keras.layers.Conv2D(k, (3, 3), (1, 1), padding="same")(input_layer)
     d_block = InstanceNorm()(d_block)
     d_block = tf.keras.layers.ReLU()(d_block)
 
     # second block
-    d2_block = tf.keras.layers.Conv2D(
-        k, (3, 3), (1, 1), padding="same", use_bias=False
-    )(d_block)
+    d2_block = tf.keras.layers.Conv2D(k, (3, 3), (1, 1), padding="same")(d_block)
     d2_block = InstanceNorm()(d2_block)
     d2_block = tf.keras.layers.ReLU()(d2_block)
 
@@ -51,29 +38,23 @@ def resi_block(input_layer, k):
 
 
 def generator(nlabel):
-    img_input = tf.keras.layers.Input(shape=(None, None, 3))
-    cond_input = tf.keras.layers.Input((None, None, nlabel))
+    img_input = tf.keras.layers.Input(shape=(128, 128, 3))
+    cond_input = tf.keras.layers.Input((128, 128, nlabel))
 
     combined_input = tf.keras.layers.Concatenate()([img_input, cond_input])
 
     # c7s1-64
-    conv1 = tf.keras.layers.Conv2D(64, (7, 7), 1, padding="same", use_bias=False)(
-        combined_input
-    )
+    conv1 = tf.keras.layers.Conv2D(64, (7, 7), 1, padding="same")(combined_input)
     conv1 = InstanceNorm()(conv1)
     conv1 = tf.keras.layers.ReLU()(conv1)
 
     # d128
-    conv2 = tf.keras.layers.Conv2D(128, (4, 4), 2, padding="same", use_bias=False)(
-        conv1
-    )
+    conv2 = tf.keras.layers.Conv2D(128, (4, 4), 2, padding="same")(conv1)
     conv2 = InstanceNorm()(conv2)
     conv2 = tf.keras.layers.ReLU()(conv2)
 
     # d256
-    conv3 = tf.keras.layers.Conv2D(256, (4, 4), 2, padding="same", use_bias=False)(
-        conv2
-    )
+    conv3 = tf.keras.layers.Conv2D(256, (4, 4), 2, padding="same")(conv2)
     conv3 = InstanceNorm()(conv3)
     conv3 = tf.keras.layers.ReLU()(conv3)
 
@@ -83,23 +64,17 @@ def generator(nlabel):
         res = resi_block(res, 256)
 
     # u128
-    deconv1 = tf.keras.layers.Conv2DTranspose(
-        128, (3, 3), 2, padding="same", use_bias=False
-    )(res)
+    deconv1 = tf.keras.layers.Conv2DTranspose(128, (3, 3), 2, padding="same")(res)
     deconv1 = InstanceNorm()(deconv1)
     deconv1 = tf.keras.layers.ReLU()(deconv1)
 
     # u64
-    deconv2 = tf.keras.layers.Conv2DTranspose(
-        64, (3, 3), 2, padding="same", use_bias=False
-    )(deconv1)
+    deconv2 = tf.keras.layers.Conv2DTranspose(64, (3, 3), 2, padding="same")(deconv1)
     deconv2 = InstanceNorm()(deconv2)
     deconv2 = tf.keras.layers.ReLU()(deconv2)
 
     # c7s1-3
-    conv4 = tf.keras.layers.Conv2D(3, (7, 7), 1, padding="same", use_bias=False)(
-        deconv2
-    )
+    conv4 = tf.keras.layers.Conv2D(3, (7, 7), 1, padding="same")(deconv2)
     conv4 = InstanceNorm()(conv4)
     conv4 = tf.keras.layers.Activation("tanh")(conv4)
 
@@ -108,7 +83,6 @@ def generator(nlabel):
     )
 
 
-# check discriminator output
 def discriminator(width, nlabel):
     input_layer = tf.keras.layers.Input((128, 128, 3))
     output = input_layer
@@ -119,16 +93,15 @@ def discriminator(width, nlabel):
         )
         output = tf.keras.layers.LeakyReLU(0.01)(output)
 
-    src_output = tf.keras.layers.Conv2D(
-        1, (3, 3), (1, 1), padding="same", use_bias=False, name="src"
-    )(output)
+    src_output = tf.keras.layers.Conv2D(1, (3, 3), (1, 1), padding="same", name="src")(
+        output
+    )
 
     cls_output = tf.keras.layers.Conv2D(
         nlabel,
         (width // 64, width // 64),
         (1, 1),
         padding="valid",
-        use_bias=False,
         name="cls",
     )(output)
 
@@ -136,15 +109,37 @@ def discriminator(width, nlabel):
         inputs=[input_layer], outputs=[src_output, cls_output], name="discriminator"
     )
 
+class LRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, init_lr):
+        self.init_lr = init_lr
+    
+    def __call__(self, step):
+        return self.init_lr / (step + 1)
 
 class Stargan:
     def __init__(
-        self, summary, width, nlabel, lambda_cls=1, lambda_rec=10, lambda_gp=10
+        self,
+        summary,
+        width,
+        nlabel,
+        lambda_cls=1,
+        lambda_rec=10,
+        lambda_gp=10,
+        scheduler=False,
     ):
         self.discriminator = discriminator(width, nlabel)
         self.generator = generator(nlabel)
-        self.disopti = tf.keras.optimizers.Adam(1e-4, 0.5)
-        self.genopti = tf.keras.optimizers.Adam(1e-4, 0.5)
+
+        if scheduler:
+            schedule = 
+
+
+            self.disopti = tf.keras.optimizers.Adam(1e-4, 0.5)
+            self.genopti = tf.keras.optimizers.Adam(1e-4, 0.5)
+        else:
+            self.disopti = tf.keras.optimizers.Adam(1e-4, 0.5)
+            self.genopti = tf.keras.optimizers.Adam(1e-4, 0.5)
+
         self.summary: tf.summary.SummaryWriter = summary
         self.binloss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
         self.width = width
@@ -152,8 +147,6 @@ class Stargan:
         self.lambda_cls = float(lambda_cls)
         self.lambda_rec = float(lambda_rec)
         self.lambda_gp = float(lambda_gp)
-
-        self.discriminator.summary()
 
     # @tf.function
     def train(self, dataset, checkpoint, batch_size):
